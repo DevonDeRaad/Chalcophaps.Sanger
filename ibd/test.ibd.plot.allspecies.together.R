26d710987af26b1bbad8cd05bd54ed3c0b365acc
Devon DeRaad <deraad@oxy.edu>
2021-07-10 11:06:40 -0400

    Add files via upload

--- ibd/test.ibd.plot.allspecies.together.R
+++ ibd/test.ibd.plot.allspecies.together.R
@@ -2,6 +2,8 @@ library(geosphere)
 library(phangorn)
 library(MASS)
 library(ade4)
+library(scales)
+library(viridis)
 
 
 ### Isolation by distance
@@ -91,7 +93,15 @@ abline(lm(stephani.gendist~stephani.Dgeo), lty =2)
 title("Correlation of Genetic and Geographical distances")
 
 
-pdf(file = "~/Dropbox/Chalcophaps/spring.2020/ibd.byspecies.pdf",width =5,height = 5)
+#plot
+pdf(file = "~/Dropbox/Chalcophaps/spring.2020/ibd.byspecies.pdf",width =6.5,height = 6)
+
+layout.matrix <- matrix(c(1,2,1,3,1,4), nrow = 2, ncol = 3)
+
+layout(mat = layout.matrix,
+       heights = c(1,.75), # Heights of the two rows
+       widths = c(1,1,1)) # Widths of the three columns
+
 #plot them all together and color points by species
 plot(indica.Dgeo, indica.gendist, pch=20,cex=1.5, ylab = "Uncorrected 'p' Distance",
      xlab = "Geographic Distance (km)", ylim=c(0,.03), col = alpha("palegreen2", 0.6), frame.plot = FALSE)
@@ -111,6 +121,112 @@ legend("topright",
        text.col = "black", 
        horiz = F)
 
+#plot residuals
+steph.lm<-lm(stephani.gendist~stephani.Dgeo)
+steph.res = resid(steph.lm)
+plot(stephani.Dgeo, steph.res, ylab="Residuals", xlab="Geographic distance", 
+     main=expression(italic('stephani')), col = alpha("sienna2", 0.6), pch=16) 
+  abline(0, 0)
+  
+longi.lm<-lm(longi.gendist~longi.Dgeo)
+longi.res = resid(longi.lm)
+plot(longi.Dgeo, longi.res, ylab="Residuals", xlab="Geographic distance", 
+     main=expression(italic('longirostris')), col = alpha("skyblue1", 0.6), pch=16) 
+abline(0, 0)   
+
+indi.lm<-lm(indica.gendist~indica.Dgeo)
+indi.res = resid(indi.lm)
+plot(indica.Dgeo, indi.res, ylab="Residuals", xlab="Geographic distance", 
+     main=expression(italic('indica')), col = alpha("palegreen2", 0.6), pch=16) 
+abline(0, 0)   
+
 dev.off()
+
+
+
+
+
+
+
+
+
+#alternatively, if you want to plot each cluster separately
+#separately plot relationship within each pop cluster for stephani (start with solomons)
+#test IBD stephani start with dgeo
+sols.spec<-stephani.spec[stephani.spec$Country == "Solomon Islands",]
+sols.xy.coords.only<- subset(sols.spec, select=c("Long","Lat"))
+sols.Dgeo <- as.dist(distm(sols.xy.coords.only, fun=distGeo))
+sols.Dgeo<-sols.Dgeo/1000
+sols.Dgeo
+
+#now genetic matrix
+#create subset for each species matching the order of the geographic dataframe
+sols.concat <- subset(all_concat, subset = c(79:84))
+#create distance matrix based on all genes concatenated
+sols.gendist<-dist.hamming(sols.concat, ratio = TRUE)
+IBD <- mantel.randtest(sols.Dgeo,sols.gendist)
+IBD
+plot(sols.Dgeo,sols.gendist, pch=20,cex=1.5, ylab = "Genetic Distance", xlab = "Geographic Distance")
+abline(lm(sols.gendist~sols.Dgeo), lty = 2)
+
+
+#separately plot relationship within each pop cluster for stephani (now PNG without New Britain)
+#test IBD stephani start with dgeo
+png.spec<-stephani.spec[stephani.spec$Country == "Papua New Guinea",]
+png.spec<-png.spec[-4,]
+png.xy.coords.only<- subset(png.spec, select=c("Long","Lat"))
+png.Dgeo <- as.dist(distm(png.xy.coords.only, fun=distGeo))
+png.Dgeo<-png.Dgeo/1000
+png.Dgeo
+
+#now genetic matrix
+#create subset for each species matching the order of the geographic dataframe
+png.concat <- subset(all_concat, subset = c(70,71,78,73,74,75,76,77))
+#create distance matrix based on all genes concatenated
+png.gendist<-dist.hamming(png.concat, ratio = TRUE)
+IBD <- mantel.randtest(png.Dgeo,png.gendist)
+IBD
+plot(png.Dgeo,png.gendist, pch=20,cex=1.5, ylab = "Genetic Distance", xlab = "Geographic Distance")
+abline(lm(png.gendist~png.Dgeo), lty = 2)
+
+#plot both together
+plot(png.Dgeo, png.gendist, pch=20,cex=1.5, ylab = "Uncorrected 'p' Distance",
+     xlab = "Geographic Distance (km)", ylim=c(0,.0012), col = "#D54941FF", frame.plot = FALSE,
+     main=expression( italic(stephani)))
+points(sols.Dgeo, sols.gendist, pch=20,cex=1.5, col="#FBBE22FF")
+abline(lm(png.gendist~png.Dgeo), lty = 1, lwd=4, col="#D54941FF")
+abline(lm(sols.gendist~sols.Dgeo), lty =1, lwd=4, col="#FBBE22FF")
+legend("topleft",
+       legend = c("PNG", "Solomon Islands"),
+       #text.font = 3,
+       col = c("#D54941FF","#FBBE22FF"), 
+       pch = c(20), 
+       bty = "n", 
+       pt.cex = 3, 
+       cex = 1.2, 
+       text.col = "black", 
+       horiz = F)
+
+
+#repeat the same for longirostris
+#separately plot relationship within each pop cluster for longi (start with solomons)
+#test IBD stephani start with dgeo
+sols.spec<-stephani.spec[stephani.spec$Country == "Solomon Islands",]
+sols.xy.coords.only<- subset(sols.spec, select=c("Long","Lat"))
+sols.Dgeo <- as.dist(distm(sols.xy.coords.only, fun=distGeo))
+sols.Dgeo<-sols.Dgeo/1000
+sols.Dgeo
+
+#now genetic matrix
+#create subset for each species matching the order of the geographic dataframe
+sols.concat <- subset(all_concat, subset = c(79:84))
+#create distance matrix based on all genes concatenated
+sols.gendist<-dist.hamming(sols.concat, ratio = TRUE)
+IBD <- mantel.randtest(sols.Dgeo,sols.gendist)
+IBD
+plot(sols.Dgeo,sols.gendist, pch=20,cex=1.5, ylab = "Genetic Distance", xlab = "Geographic Distance")
+abline(lm(sols.gendist~sols.Dgeo), lty = 2)
+
+
 
 
